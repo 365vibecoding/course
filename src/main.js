b868aa5cb80ccd03b20ab3f981b30c787d8e3e88
park363 <park363@naver.com>
2026-02-08 17:06:23 +0900

    course data

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import './style.css'
+import './board.css'
+
+const res = await fetch("001/001-01-01.html")
+const text = await res.text()
+console.log(text)
+document.querySelector('#app').innerHTML = text
+
+// document.querySelector('#app').innerHTML = `
+//   <div>
+//     <a href="https://vite.dev" target="_blank">
+//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
+//     </a>
+//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+//     </a>
+//     <h1>Hello Vite!</h1>
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//     <p class="read-the-docs">
+//       Click on the Vite logo to learn more
+//     </p>
+//   </div>
+// `
+// setupCounter(document.querySelector('#counter'))
